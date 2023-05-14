e7d343d3cdf4d8f456e420802f196bf25f1ab12b
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-14 22:00:27 +0300

    feat (LoginPage) added LoginPage.tsx

---
+++ src/ui/components/Login/LoginPage.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {useAppDispatch, useAppSelector} from '../../../bll/store/hook';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import {loginTC} from '../../../bll/slices/auth-slice';
+import {PATH} from '../../../utils/path/PATH';
+import {Navigate} from 'react-router-dom';
+
+export const LoginPage = () => {
+    const dispatch = useAppDispatch();
+    const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn);
+
+    const formik = useFormik({
+        initialValues: {
+            idInstance: '',
+            apiTokenInstance: '',
+        },
+        validationSchema: Yup.object().shape({
+            idInstance: Yup.string()
+                .required('Required'),
+            apiTokenInstance: Yup.string()
+                .min(4, 'Too Short!')
+                .max(50, 'Too Long!')
+                .required('Required'),
+        }),
+        onSubmit: values => {
+            console.log(`before dispatch ${isLoggedIn}`)
+            dispatch(loginTC(values.idInstance, values.apiTokenInstance));
+            console.log(`after dispatch ${isLoggedIn}`)
+        },
+    });
+
+    if (isLoggedIn) {
+        return <Navigate to={PATH.MAIN}/>;
+    }
+
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <input
+                        type="idInstance"
+                        id="idInstance"
+                        placeholder="idInstance"
+                        {...formik.getFieldProps('idInstance')}
+                    />
+                    {formik.touched.idInstance && formik.errors.idInstance ?
+                        <div>{formik.errors.idInstance}</div> : null}
+                </div>
+                <div>
+                    <input
+                        type="apiTokenInstance"
+                        id="apiTokenInstance"
+                        placeholder="apiTokenInstance"
+                        {...formik.getFieldProps('apiTokenInstance')}
+                    />
+                    {formik.touched.apiTokenInstance && formik.errors.apiTokenInstance ? (
+                        <div>{formik.errors.apiTokenInstance}</div>
+                    ) : null}
+                </div>
+                <button>Login</button>
+            </form>
+        </div>
+    );
+};
+
