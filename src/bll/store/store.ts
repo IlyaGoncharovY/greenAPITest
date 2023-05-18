7d5dd9c39d5646e8dc4d09f51701da85217dee71
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-18 04:10:41 +0300

    fix (store) added chat and message reducer

--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,12 +1,16 @@
 import {Action, configureStore, ThunkAction} from "@reduxjs/toolkit";
 import authorizedReducer from "../slices/app-slice"
 import AuthReducer from "../slices/auth-slice";
+import ChatSlice from "../slices/chat-slice";
+import MessageSlice from "../slices/message-slice";
 
 
 export const store = configureStore({
     reducer: {
         app: authorizedReducer,
-        auth: AuthReducer
+        auth: AuthReducer,
+        chat: ChatSlice,
+        message: MessageSlice
     }
 });
 
