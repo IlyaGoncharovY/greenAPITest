8d66f206843bfe8c876d4b92d86e52d2e3ad4b7d
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-05-18 04:36:35 +0300

    fix (use defaultAva) from image for avatar

--- src/ui/components2/Chat/AddChatContaner/AddChat/Item/ItemUser.tsx
+++ src/ui/components2/Chat/AddChatContaner/AddChat/Item/ItemUser.tsx
@@ -2,6 +2,7 @@ import React, {FC} from 'react';
 import {removeUser, setActiveChat} from "../../../../../../bll/slices/chat-slice";
 import {useAppDispatch} from "../../../../../../bll/store/hook";
 import {ResponseContactInfoType} from "../../../../../../api/chatAPI";
+import defaultAva from "../../../../../../assects/defaultAva.png"
 
 interface ItemUserType {
     user: ResponseContactInfoType
@@ -21,7 +22,7 @@ export const ItemUser: FC<ItemUserType> = ({user}) => {
 
     return (
         <div onClick={() => clickActivateChat(user.chatId)}>
-            <img src={user.avatar} alt={"avatar"} style={{width: "30px"}}/>
+            <img src={user.avatar ? user.avatar : defaultAva} alt={"avatar"} style={{width: "40px"}}/>
             {user.name}
             <button onClick={() => clickDeleteHandler(user.chatId)}>delete</button>
         </div>
--- src/ui/components2/Chat/DsplayChatContaner/Header/HeaderBodyChat.tsx
+++ src/ui/components2/Chat/DsplayChatContaner/Header/HeaderBodyChat.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {ResponseContactInfoType} from "../../../../../api/chatAPI";
+import defaultAva from "../../../../../assects/defaultAva.png"
 
 interface HeaderBodyChatType {
     users: ResponseContactInfoType[];
@@ -16,7 +17,8 @@ export const HeaderBodyChat: FC<HeaderBodyChatType> = ({users, activateChatId}) 
         <div>
             {activateChatId && getInfoContact() ? (
                 <>
-                    <img src={getInfoContact().avatar} alt={"avatar"} style={{width:"30px"}}/>
+                    <img src={getInfoContact().avatar ? getInfoContact().avatar : defaultAva}
+                         alt={"avatar"} style={{width:"40px"}}/>
                     {getInfoContact().name ? getInfoContact().name : getInfoContact().chatId}
                 </>
             ) : null}
